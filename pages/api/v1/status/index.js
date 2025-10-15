f9e0d4c619a675feb86cbcc331b3c6c7f45be073
Gustavo Cotta <gmcotta34@gmail.com>
2025-10-15 11:01:46 -0300

    feat(infra): inicia o modulo database

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-export default function status(req, res) {
+import database from '../../../../infra/database.js';
+
+export default async function status(req, res) {
+  const result = await database.query('SELECT 1 + 1;');
+  console.log(result);
   // res.status(200).send('curso.dev - status está ok');
   res.status(200).json({ teste: 'curso.dev - status está ok' });
 }
