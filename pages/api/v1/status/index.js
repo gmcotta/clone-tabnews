3a2d0012d10b66fc32f22a16e152556fad09c622
Gustavo Cotta <gmcotta34@gmail.com>
2025-10-17 11:25:33 -0300

    feat(api/status): adiciona status do db

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,32 @@
-import database from 'infra/database.js';
+import database from "infra/database.js";
 
 export default async function status(req, res) {
-  const result = await database.query('SELECT 1 + 1;');
-  console.log(result);
-  // res.status(200).send('curso.dev - status está ok');
-  res.status(200).json({ teste: 'curso.dev - status está ok' });
+  const updatedAt = new Date().toISOString();
+
+  const versionQueryResult = await database.query(`SHOW server_version;`);
+  const version = versionQueryResult.rows[0].server_version;
+
+  const maxConnectionsQueryResult = await database.query(
+    `SHOW max_connections;`,
+  );
+  const maxConnections = maxConnectionsQueryResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const currentConnectionsQueryResult = await database.query({
+    text: `SELECT count(*)::int AS current_connections FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+  const currentConnections =
+    currentConnectionsQueryResult.rows[0].current_connections;
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version,
+        max_connections: parseInt(maxConnections),
+        current_connections: currentConnections,
+      },
+    },
+  });
 }
