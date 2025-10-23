834fdf55ea18c2919c73eaf2f93d8fcae6a9c649
Gustavo Cotta <gmcotta34@gmail.com>
2025-10-23 10:10:50 -0300

    test(migrations): cria testes de GET e POST da rota

---
+++ tests/integration/api/v1/migrations/get.test.js
@@ -0,0 +1,20 @@
+import database from "infra/database";
+
+async function cleanDatabase() {
+  await database.query('DROP SCHEMA public CASCADE; CREATE SCHEMA public;')
+}
+
+beforeAll(async () => {
+  await cleanDatabase()
+})
+
+describe("/migrations", () => {
+  it("should return 200 when GET to /api/v1/migrations", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/migrations");
+    expect(response.status).toBe(200);
+
+    const responseBody = await response.json();
+    expect(Array.isArray(responseBody)).toBe(true);
+    expect(responseBody.length).toBeGreaterThan(0);
+  });
+});
---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,31 @@
+import database from "infra/database";
+
+async function cleanDatabase() {
+  await database.query('DROP SCHEMA public CASCADE; CREATE SCHEMA public;')
+}
+
+beforeAll(async () => {
+  await cleanDatabase()
+})
+
+describe("/migrations", () => {
+  it("should return 200 when POST to /api/v1/migrations", async () => {
+    const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+      method: "POST",
+    });
+    expect(response1.status).toBe(201);
+
+    const responseBody1 = await response1.json();
+    expect(Array.isArray(responseBody1)).toBe(true);
+    expect(responseBody1.length).toBeGreaterThan(0);
+
+    const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+      method: "POST",
+    });
+    expect(response2.status).toBe(200);
+    const responseBody2 = await response2.json();
+
+    expect(Array.isArray(responseBody2)).toBe(true);
+    expect(responseBody2.length).toBe(0);
+  });
+});
