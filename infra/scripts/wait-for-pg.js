d171d41e3dab55eedd57af33783b9f19cfbc0f20
Gustavo Cotta <gmcotta34@gmail.com>
2025-10-28 10:00:46 -0300

    feat: cria script wait-for-pg

---
+++ infra/scripts/wait-for-pg.js
@@ -0,0 +1,19 @@
+const { exec } = require("node:child_process");
+
+function checkPostgres() {
+  exec(`docker exec postgres_dev pg_isready --host localhost`, handleReturn);
+
+  function handleReturn(error, stdout, stderr) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    }
+
+    console.log("\nPostgres está pronto e aceitando conexões!");
+  }
+}
+
+process.stdout.write("Aguardando Postgres ficar pronto");
+
+checkPostgres();
