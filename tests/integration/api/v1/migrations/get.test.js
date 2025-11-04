754366f4fab36be0a9931e8385d9a159933ea32b
Gustavo Cotta <gmcotta34@gmail.com>
2025-11-04 10:17:55 -0300

    test: deixa os testes mais descritivos

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,13 +6,15 @@ beforeAll(async () => {
   await cleanDatabase();
 });
 
-describe('/migrations', () => {
-  it('should return 200 when GET to /api/v1/migrations', async () => {
-    const response = await fetch('http://localhost:3000/api/v1/migrations');
-    expect(response.status).toBe(200);
+describe('GET /api/v1/migrations', () => {
+  describe('Anonymous user', () => {
+    it('should retrieve pending migrations', async () => {
+      const response = await fetch('http://localhost:3000/api/v1/migrations');
+      expect(response.status).toBe(200);
 
-    const responseBody = await response.json();
-    expect(Array.isArray(responseBody)).toBe(true);
-    expect(responseBody.length).toBeGreaterThan(0);
+      const responseBody = await response.json();
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
   });
 });
